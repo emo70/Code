2e458af02f5f6f4b9a75e6900602b20db5128d7a
Emeka Okonkwo <g00255702@stu.raritanval.edu>
2018-12-03 10:39:58 -0500

    replace

--- CPU Scheduling/FCFS.java
+++ CPU Scheduling/FCFS.java
@@ -12,66 +12,66 @@ public class FCFS {
  
  public void FCFS()
  {
-       int n;
-       n = 10;
-       int proc[][] = new int[n + 1][4]; //Array of 10 Processes
+       int n;//Process
+       n = 10;// Set to 10 Processes
+       int process[][] = new int[n+1][4]; //Array of Processes
        System.out.println("\nCPU Scheduling using the First Come First Serve Algorithm\n");
        System.out.println("Process ID\t Burst");
        for(int i = 1; i <= n; i++)
        {
-           proc[i][1] = rand.nextInt(20)+1; //Random Set of Burst for each process
-           System.out.printf("%d\t%10dms\n",i,proc[i][1]);
+           process[i][1] = rand.nextInt(20)+1; //Random Set of Burst for each processess between 1 and 20
+           System.out.printf("%d\t%10dms\n",i,process[i][1]);
         }
        
-     //Calculation of Total Time and Initialization of Time Chart array
+     //Calculation of Total Time and Initialization of Gantt Chart array
      int total_time = 0;
      for(int i = 1; i <= n; i++)
      {
-      total_time += proc[i][1];
+      total_time += process[i][1];
      }
-     int time_chart[] = new int[total_time];
+     int gantt_chart[] = new int[total_time];
      
-     int sel_proc = 1;
+     int sel_process = 1;
      
      for(int i = 0; i < total_time; i++)
      { 
-      //Assign selected process to current time in the Chart
-      time_chart[i] = sel_proc;
+      //Assign selected processess to current time in the Chart
+      gantt_chart[i] = sel_process;
       
-      //Decrement Remaining Time of selected process by 1 since it has been assigned the CPU for 1 unit of time
-      proc[sel_proc][1]--;
+      //Decrement Remaining Time of selected processess by 1 since it has been assigned the CPU for 1 unit of time
+      process[sel_process][1]--;
       
       //WT and TT Calculation
       for(int j = 1; j <= n; j++)
       {
-       if(proc[j][1] != 0)
+       if(process[j][1] != 0)
        {
-        proc[j][3]++;//If process has completed execution its TT is incremented by 1
-        if(j != sel_proc)//If the process has not been currently assigned the CPU its WT is incremented by 1
-         proc[j][2]++;
+        process[j][3]++;//If processess has completed execution its TT is incremented by 1
+        if(j != sel_process)//If the processess has not been currently assigned the CPU its WT is incremented by 1
+         process[j][2]++;
        }
-       else if(j == sel_proc)//This is a special case in which the process has been assigned CPU and has completed its execution
-        proc[j][3]++;
+       else if(j == sel_process)//This is a special case in which the processess has been assigned CPU and has completed its execution
+        process[j][3]++;
       }
       
-      //Printing the Time Chart
+      //Printing the Gantt Chart
       if(i != 0)
       {
-       if(sel_proc != time_chart[i - 1])
+       if(sel_process != gantt_chart[i - 1])
         //If the CPU has been assigned to a different Process we need to print the current value of time and the name of 
         //the new Process
        {
-        System.out.print("--" + i + "--P" + sel_proc);
+        System.out.print("--" + i + "--P" + sel_process);
        }
       }
       else//If the current time is 0 i.e the printing has just started we need to print the name of the First selected Process
-       System.out.print(i + "--P" + sel_proc);
-      if(i == total_time - 1)//All the process names have been printed now we have to print the time at which execution ends
+       System.out.print(i + "--P" + sel_process);
+      if(i == total_time - 1)//All the processess names have been printed now we have to print the time at which execution ends
        System.out.print("--" + (i + 1));
       
-      //If current process has been completed we select the next process from the list
-      if(proc[sel_proc][1] == 0)
-       sel_proc++;
+      //If current processess has been completed we select the next processess from the list
+      if(process[sel_process][1] == 0)
+       sel_process++;
       
      }
      System.out.println();
@@ -81,7 +81,7 @@ public class FCFS {
      System.out.println("Process ID\t WT  \t    TT  ");
      for(int i = 1; i <= n; i++)
      {
-      System.out.printf("%d\t%10dms\t%5dms",i,proc[i][2],proc[i][3]);
+      System.out.printf("%d\t%10dms\t%5dms",i,process[i][2],process[i][3]);
       System.out.println();
      }
      
@@ -91,8 +91,8 @@ public class FCFS {
      float WT = 0,TT = 0;
      for(int i = 1; i <= n; i++)
      {
-      WT += proc[i][2];
-      TT += proc[i][3];
+      WT += process[i][2];
+      TT += process[i][3];
      }
      WT /= n;
      TT /= n;
--- CPU Scheduling/SRTF.java
+++ CPU Scheduling/SRTF.java
@@ -10,79 +10,79 @@ public class SRTF {
     private Random rand = new Random();
  public void SRTF() throws IOException
  {
-      int n;
-       n = 10;
-       int proc[][] = new int[n + 1][4];
+      int n;//Process
+       n = 10;//Set to 10 Processes
+       int process[][] = new int[n + 1][4];
        System.out.println("\nCPU Scheduling using the Shortest Remaining Time First Algorithm\n");
        System.out.println("Process ID\t Arrival Time \t Burst");
        for(int i = 1; i <= n; i++)
        {
-      proc[i][0] = rand.nextInt(10);//Arrival Time
-      proc[i][1] = rand.nextInt(20)+1;//Burst 
-      System.out.printf("%d\t%10dms\t%10dms\n",i,proc[i][0], proc[i][1]);
+      process[i][0] = rand.nextInt(10);//Arrival Time
+      process[i][1] = rand.nextInt(20)+1;//Random Set of Burst for each processess between 1 and 20 
+      System.out.printf("%d\t%10dms\t%10dms\n",i,process[i][0], process[i][1]);
      }
      System.out.println();
      
-       //Calculation of Total Time and Initialization of Time Chart array
+       //Calculation of Total Time and Initialization of Gantt Chart array
      int total_time = 0;
      for(int i = 1; i <= n; i++)
      {
-      total_time += proc[i][1];
+      total_time += process[i][1];
      }
-     int time_chart[] = new int[total_time];
+     int gantt_chart[] = new int[total_time];
      
      for(int i = 0; i < total_time; i++)
      {
-      //Selection of shortest process which has arrived
-      int sel_proc = 0;
+      //Selection of shortest processess which has arrived
+      int sel_process = 0;
       int min = 99999;
       for(int j = 1; j <= n; j++)
       {
-       if(proc[j][0] <= i)//Condition to check if Process has arrived
+       if(process[j][0] <= i)//Condition to check if Process has arrived
        {
-        if(proc[j][1] < min && proc[j][1] != 0)
+        if(process[j][1] < min && process[j][1] != 0)
         {
-         min = proc[j][1];
-         sel_proc = j;
+         min = process[j][1];
+         sel_process = j;
         }
        }
       }
       
-      //Assign selected process to current time in the Chart
-      time_chart[i] = sel_proc;
+      //Assign selected processess to current time in the Chart
+      gantt_chart[i] = sel_process;
       
-      //Decrement Remaining Time of selected process by 1 since it has been assigned the CPU for 1 unit of time
-      proc[sel_proc][1]--;
+      //Decrement Remaining Time of selected processess by 1 since it has been assigned the CPU for 1 unit of time
+      process[sel_process][1]--;
       
       //WT and TT Calculation
       for(int j = 1; j <= n; j++)
       {
-       if(proc[j][0] <= i)
+       if(process[j][0] <= i)
        {
-        if(proc[j][1] != 0)
+        if(process[j][1] != 0)
         {
-         proc[j][3]++;//If process has arrived and it has not already completed execution its TT is incremented by 1
-            if(j != sel_proc)//If the process has not been currently assigned the CPU and has arrived its WT is incremented by 1
-             proc[j][2]++;
+         process[j][3]++;//If processess has arrived and it has not already completed execution its TT is incremented by 1
+            if(j != sel_process)//If the processess has not been currently assigned the CPU and has arrived its WT is incremented by 1
+             process[j][2]++;
         }
-        else if(j == sel_proc)//This is a special case in which the process has been assigned CPU and has completed its execution
-         proc[j][3]++;
+        else if(j == sel_process)//This is a special case in which the processess has been assigned CPU and has completed its execution
+         process[j][3]++;
        }
       }
       
-      //Printing the Time Chart
+      //Printing the Gantt Chart
       if(i != 0)
       {
-       if(sel_proc != time_chart[i - 1])
+       if(sel_process != gantt_chart[i - 1])
         //If the CPU has been assigned to a different Process we need to print the current value of time and the name of 
         //the new Process
        {
-        System.out.print("--" + i + "--P" + sel_proc);
+        System.out.print("--" + i + "--P" + sel_process);
        }
       }
       else//If the current time is 0 i.e the printing has just started we need to print the name of the First selected Process
-       System.out.print(i + "--P" + sel_proc);
-      if(i == total_time - 1)//All the process names have been printed now we have to print the time at which execution ends
+       System.out.print(i + "--P" + sel_process);
+      if(i == total_time - 1)//All the processess names have been printed now we have to print the time at which execution ends
        System.out.print("--" + (i + 1));
       
      }
@@ -93,7 +93,7 @@ public class SRTF {
      System.out.println("Process ID\t WT  \t    TT  ");
      for(int i = 1; i <= n; i++)
      {
-      System.out.printf("%d\t%10dms\t%5dms",i,proc[i][2],proc[i][3]);
+      System.out.printf("%d\t%10dms\t%5dms",i,process[i][2],process[i][3]);
       System.out.println();
      }
      
@@ -103,8 +103,8 @@ public class SRTF {
      float AWT = 0,ATT = 0;
      for(int i = 1; i <= n; i++)
      {
-      AWT += proc[i][2];
-      ATT += proc[i][3];
+      AWT += process[i][2];
+      ATT += process[i][3];
      }
      AWT /= n;
      ATT /= n;
